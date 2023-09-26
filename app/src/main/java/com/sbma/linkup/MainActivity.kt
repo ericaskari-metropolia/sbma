c42b2275a0cd22a8b79a9f14822e2be89b1dd97d
ericaskari <ericaskari@gmail.com>
2023-09-26 13:40:06 +0300

    Contact Screen Basic

--- app/src/main/java/com/sbma/linkup/MainActivity.kt
+++ app/src/main/java/com/sbma/linkup/MainActivity.kt
@@ -10,17 +10,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.sbma.linkup.presentation.screens.UserConnectionsScreenProvider
 import com.sbma.linkup.ui.theme.LinkUpTheme
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController: NavHostController = rememberNavController()
+
             LinkUpTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") { backStackEntry ->
+                        // A surface container using the 'background' color from the theme
+                        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                            Greeting("Android")
+                        }
+                    }
+                    composable(
+                        "users/{userId}/contacts",
+                        arguments = listOf(navArgument("userId") { type = NavType.StringType }),
+                    ) { backStackEntry ->
+                        backStackEntry.arguments?.getString("userId")?.let {
+                            UserConnectionsScreenProvider(
+                                userId = UUID.fromString(it))
+                        }
+
+                    }
+
                 }
+
+
             }
         }
     }
---
+++ app/src/main/java/com/sbma/linkup/presentation/screenstates/UserConnectionsScreenState.kt
@@ -0,0 +1,7 @@
+package com.sbma.linkup.presentation.screenstates
+
+import com.sbma.linkup.user.User
+
+data class UserConnectionsScreenState(
+    val contacts: List<User>
+)
