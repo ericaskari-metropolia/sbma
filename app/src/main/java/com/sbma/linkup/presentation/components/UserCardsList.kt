14dc8f5c706d0c4428ca36cdfe25eb4570604f97
ericaskari <ericaskari@gmail.com>
2023-09-28 11:23:32 +0300

    User profile cards

--- app/src/main/java/com/sbma/linkup/presentation/components/UserCardsList.kt
+++ app/src/main/java/com/sbma/linkup/presentation/components/UserCardsList.kt
@@ -1,32 +1,44 @@
 package com.sbma.linkup.presentation.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.sbma.linkup.application.data.AppViewModelProvider
+import com.sbma.linkup.presentation.screens.ContactInfoRow
 import com.sbma.linkup.ui.theme.LinkUpTheme
 import com.sbma.linkup.usercard.UserCard
-import com.sbma.linkup.usercard.UserCardViewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.UUID
 
 @Composable
 fun UserCardsListItem(userCard: UserCard) {
-    Column {
-        Text(userCard.name)
-        Text(userCard.value)
-    }
+    ListItem(
+        overlineContent = { Text(userCard.name) },
+        headlineContent = { Text(userCard.value) },
+    )
 }
 
 @Composable
