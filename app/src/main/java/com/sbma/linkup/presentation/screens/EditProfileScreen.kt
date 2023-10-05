07d45caab1d031d4897fdaae7bc092aa87b25d58
shaykandagor <shaynekandagor@gmail.com>
2023-10-05 14:02:14 +0300

    Created a edit profile bottom sheet for social media icons

--- app/src/main/java/com/sbma/linkup/presentation/screens/EditProfileScreen.kt
+++ app/src/main/java/com/sbma/linkup/presentation/screens/EditProfileScreen.kt
@@ -1,28 +1,41 @@
 package com.sbma.linkup.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -35,6 +48,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,6 +57,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import com.sbma.linkup.R
 import com.sbma.linkup.application.data.AppViewModelProvider
 import com.sbma.linkup.card.Card
 import com.sbma.linkup.card.CardViewModel
@@ -71,7 +87,7 @@ fun EditProfileScreen(
     var newCards = remember { mutableStateOf<List<Card>>(listOf()) }
     val composableScope = rememberCoroutineScope()
 
-    LaunchedEffect(true){
+    LaunchedEffect(true) {
         userCardViewModel.allItemsStream(user.id).collectLatest {
             println(it)
             var phoneNumberCard = it.find { it.title == "Phone Number" }
@@ -82,7 +98,7 @@ fun EditProfileScreen(
         }
     }
 
-    LaunchedEffect(true){
+    LaunchedEffect(true) {
         userCardViewModel.allItemsStream(user.id).collectLatest {
             println(it)
             var addressCard = it.find { it.title == "Address" }
@@ -93,7 +109,7 @@ fun EditProfileScreen(
         }
     }
 
-    LaunchedEffect(true){
+    LaunchedEffect(true) {
         userCardViewModel.allItemsStream(user.id).collectLatest {
             println(it)
             var aboutMeCard = it.find { it.title == "About Me" }
@@ -104,7 +120,7 @@ fun EditProfileScreen(
         }
     }
 
-    LaunchedEffect(true){
+    LaunchedEffect(true) {
         userCardViewModel.allItemsStream(user.id).collectLatest {
             println(it)
             var descriptionCard = it.find { it.title == "Description" }
@@ -168,7 +184,6 @@ fun EditProfileScreen(
             )
         }
 
-//        }
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -196,7 +211,7 @@ fun EditProfileScreen(
             )
             TextField(
                 value = description,
-                onValueChange = { description = it},
+                onValueChange = { description = it },
                 colors = TextFieldDefaults.textFieldColors(
                     containerColor = Color.Transparent
                 )
@@ -221,7 +236,7 @@ fun EditProfileScreen(
             )
             TextField(
                 value = phone,
-                onValueChange = { phone = it},
+                onValueChange = { phone = it },
                 colors = TextFieldDefaults.textFieldColors(
                     containerColor = Color.Transparent
                 )
@@ -267,7 +282,7 @@ fun EditProfileScreen(
             )
             TextField(
                 value = aboutMe,
-                onValueChange = {  aboutMe = it },
+                onValueChange = { aboutMe = it },
                 colors = TextFieldDefaults.textFieldColors(
                     containerColor = Color.Transparent
                 ),
@@ -280,7 +295,14 @@ fun EditProfileScreen(
 
         CreateCard(onSubmit = {
             val copy = newCards.value.toMutableList()
-            copy.add(Card(id= UUID.randomUUID(), userId = user.id, title = it.title, value = it.value))
+            copy.add(
+                Card(
+                    id = UUID.randomUUID(),
+                    userId = user.id,
+                    title = it.title,
+                    value = it.value
+                )
+            )
             newCards.value = copy
             println("CreateCard")
 
@@ -310,6 +332,85 @@ fun EditProfileScreen(
                 Text("Save", color = Color.Black)
             }
         }
+        BottomSheet()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet() {
+    val size = remember {
+        mutableStateOf(50.dp)
+    }
+
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        FloatingActionButton(
+            onClick = {
+                isSheetOpen = true
+            },
+        ) {
+            Icon(Icons.Filled.Add, "Floating action button.")
+        }
+
+    }
+    if (isSheetOpen) {
+        ModalBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { isSheetOpen = false }) {
+            SocialMediaList()
+        }
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SocialMediaItem(
+    iconRes: Int,
+    text: String
+) {
+    Column {
+        ListItem(
+            headlineContent = { Text(
+                text = text,
+            ) },
+            leadingContent = {
+                Image(
+                    painter = painterResource(iconRes),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(48.dp)
+                        .padding(8.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        )
+    }
+
+}
+
+@Composable
+fun SocialMediaList() {
+    val socialMediaList = listOf(
+        Pair(R.drawable.instagram, "Instagram"),
+        Pair(R.drawable.twitter, "Twitter"),
+        Pair(R.drawable.linkedin, "Linkedin"),
+        Pair(R.drawable.facebook, "Facebook")
+    )
+
+    LazyColumn {
+        items(socialMediaList) { (iconRes, text) ->
+            SocialMediaItem(iconRes = iconRes, text = text)
+        }
     }
 }
 
@@ -318,8 +419,19 @@ fun EditProfileScreen(
 @Composable
 fun DefaultPreview() {
     val user =
-        remember { mutableStateOf(User(UUID.randomUUID(), "Sebubebu", email = "", description = "UX/UI Designer",picture = null)) }
+        remember {
+            mutableStateOf(
+                User(
+                    UUID.randomUUID(),
+                    "Sebubebu",
+                    email = "",
+                    description = "UX/UI Designer",
+                    picture = null
+                )
+            )
+        }
     LinkUpTheme {
         EditProfileScreen(user.value, onSave = {})
+
     }
 }
