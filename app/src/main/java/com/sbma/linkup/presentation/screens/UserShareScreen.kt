503bcc5e192bc295104bda75a2c44372c907cf00
ericaskari <ericaskari@gmail.com>
2023-10-05 17:41:29 +0300

    room database update and user share screen topbar

--- app/src/main/java/com/sbma/linkup/presentation/screens/UserShareScreen.kt
+++ app/src/main/java/com/sbma/linkup/presentation/screens/UserShareScreen.kt
@@ -16,10 +16,16 @@ import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
@@ -27,6 +33,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,31 +65,42 @@ fun UserShareScreenProvider(user: User, onShareClick: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserShareScreenTopBar(scrollBehavior: TopAppBarScrollBehavior) {
 
+    MediumTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(
+                text = "What would you like to share?",
+                style = MaterialTheme.typography.labelLarge,
+                fontSize = 20.sp
+            )
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserShareScreen(userCards: List<Card>, onShareClick: (userCards: List<Card>) -> Unit) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
     val input = remember {
         mutableStateOf(userCards.map { Pair(it, false) })
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp, bottom = 80.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = "Share",
-            fontSize = 25.sp,
-            style = MaterialTheme.typography.labelLarge,
-        )
-        Text(
-            text = "What would you like to share?",
-            style = MaterialTheme.typography.labelLarge,
-            fontSize = 20.sp
-        )
-
-        LazyColumn {
+    Scaffold(
+        topBar = {
+            UserShareScreenTopBar(scrollBehavior)
+        },
+        modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection),
+    ) { padding ->
+        LazyColumn(modifier = Modifier.padding(padding)) {
             itemsIndexed(input.value) { index, mediaItem ->
                 InfoListItem(mediaItem) {
                     val copy = input.value.toMutableList()
@@ -90,26 +109,31 @@ fun UserShareScreen(userCards: List<Card>, onShareClick: (userCards: List<Card>)
                     input.value = copy
                 }
             }
+            item {
+                Button(
+                    onClick = {
+                        onShareClick(
+                            input.value
+                                .filter { pair -> pair.second }
+                                .map { it.first }
+                        )
+                    },
+                ) {
+                    Icon(
+                        Icons.Filled.Share,
+                        contentDescription = "Localized description",
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text("Share")
+                }
+            }
         }
 
-        Button(
-            onClick = {
-                onShareClick(
-                    input.value
-                        .filter { pair -> pair.second }
-                        .map { it.first }
-                )
-            },
-        ) {
-            Icon(
-                Icons.Filled.Share,
-                contentDescription = "Localized description",
-                modifier = Modifier.size(ButtonDefaults.IconSize)
-            )
-            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text("Share")
-        }
+
+
     }
+
 }
 
 
